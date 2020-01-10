37e4e12b2ac9a45c9a8530e777e1d154730ff479
Crypt <hello@cryptkpr.me>
2020-01-09 22:58:23 -0600

    enh: type Uint24 to [3]uint8 directly, avoid struct

--- pkg/uint24/uint24.go
+++ pkg/uint24/uint24.go
@@ -12,22 +12,20 @@ func NewUint24(val uint32) *Uint24 {
 	return u
 }
 
-type Uint24 struct {
-	B [3]uint8
-}
+type Uint24 [3]uint8
 
 func (u *Uint24) Set(val uint32) {
 	// panic since this is closer to how a compiler/runtime would treat an overflow compared to err returns
 	if val > MaxUint24 {
 		panic("cannot set Uint24 larger than uint24.MaxUint24")
 	}
-	u.B[0] = uint8(val & 0xFF)
-	u.B[1] = uint8((val >> 8) & 0xFF)
-	u.B[2] = uint8((val >> 16) & 0xFF)
+	(*u)[0] = uint8(val & 0xFF)
+	(*u)[1] = uint8((val >> 8) & 0xFF)
+	(*u)[2] = uint8((val >> 16) & 0xFF)
 }
 
 func (u Uint24) Uint32() uint32 {
-	return uint32(u.B[0]) | uint32(u.B[1])<<8 | uint32(u.B[2])<<16
+	return uint32(u[0]) | uint32(u[1])<<8 | uint32(u[2])<<16
 }
 
 func (u Uint24) String() string {
