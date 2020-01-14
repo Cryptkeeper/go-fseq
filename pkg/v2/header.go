80bf807fa83848e9efdb5d4334f8b8535e91a6c9
Crypt <hello@cryptkpr.me>
2020-01-13 20:14:01 -0600

    add: #ComputeMaxBlocks parody to v2.Header

--- pkg/v2/header.go
+++ pkg/v2/header.go
@@ -1,5 +1,15 @@
 package v2
 
+import (
+	"math"
+)
+
+const (
+	FppMaxCompressionBlockLength = 64 * 2014
+	FppMaxBlockCount             = math.MaxUint8
+	FppWithheldFrameCount        = 10
+)
+
 type Header struct {
 	Identifier             [4]uint8
 	ChannelDataStartOffset uint16
@@ -15,4 +25,41 @@ type Header struct {
 	SparseRangeCount       uint8
 	Flags2                 uint8 `fppignored:"true" fppdefault:"0"`
 	UniqueID               uint64
+}
+
+func (h Header) ComputeMaxBlocks(maxBlockLength, maxBlockCount, withheldFrameCount int) (blockCount, withheldFrameLength, framesPerBlock int) {
+	withheldFrameLength = int(h.ChannelCount) * withheldFrameCount
+
+	if h.FrameCount == 0 {
+		blockCount = 0
+		framesPerBlock = 0
+		return
+	}
+
+	// Instantly return if frameCount fits within the withheldFrameCount
+	var availableFrameCount = int(h.FrameCount) - withheldFrameCount
+	if availableFrameCount <= 0 {
+		blockCount = 1
+		framesPerBlock = int(h.FrameCount)
+		return
+	}
+
+	// blockCount does not include withheldFrameCount
+	blockCount = int(math.Ceil(float64(int(h.ChannelCount)*availableFrameCount) / float64(maxBlockLength)))
+
+	// If withholding frames, lower the maxBlockCount to ensure available space
+	var withheldBlockCount = 0
+	if withheldFrameCount > 0 {
+		withheldBlockCount = 1
+	}
+
+	var availableBlockCount = maxBlockCount - withheldBlockCount
+	if blockCount > availableBlockCount {
+		blockCount = availableBlockCount
+	}
+
+	// Determine framesPerBlock, ignoring withheldFrameCount
+	framesPerBlock = int(math.Ceil(float64(availableFrameCount) / float64(blockCount)))
+
+	return
 }
