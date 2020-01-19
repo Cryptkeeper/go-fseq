1c7bac688619f53c58446110312b3cd71a9101b2
Crypt <hello@cryptkpr.me>
2020-01-19 00:29:58 -0600

    ref: move #ValidIdentifier to fseq.go

--- fseq.go
+++ fseq.go
@@ -1,5 +1,7 @@
 package fseq
 
+import "bytes"
+
 const (
 	majorVersionIndex = 7
 	minorVersionIndex = 6
@@ -8,4 +10,18 @@ const (
 func ReadVersion(b []byte) (minorVersion, majorVersion uint8) {
 	_ = b[majorVersionIndex] // early bounds check to guarantee safety of reads below
 	return b[minorVersionIndex], b[majorVersionIndex]
+}
+
+var ValidIdentifiers = [][]uint8{
+	[]uint8("PESQ"),
+	[]uint8("FSEQ"),
+}
+
+func ValidIdentifier(id [4]uint8) bool {
+	for _, v := range ValidIdentifiers {
+		if bytes.Equal(v, id[:]) {
+			return true
+		}
+	}
+	return false
 }
